9676d42fdc52a9423df8e313fdea561865b94a23
Youstina <youstina@gmail.com>
2023-10-07 17:08:16 -0700

    0X0A C - Argc, Argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,46 +1,51 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - print coins for an amount of money
- * @argc: argument counter
- * @argv: argument vector
- * Return: coins, 0 if negative, 1 not given
- */
-
+ * main - Main Entry
+ * @argc: input
+ * @argv: input
+ * Return: Always 0 (success)
+**/
 int main(int argc, char *argv[])
 {
-	int n;
-	int coins = 0;
+int cents, coins = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	if (argv[1][0] == 45)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	n = atoi(argv[1]);
-	coins += n / 25;
-
-	n = n % 25;
-	coins += n / 10;
-
-	n = n % 10;
-	coins += n / 5;
-
-	n = n % 5;
-	coins += n / 2;
-
-	n = n % 2;
-	coins += n / 1;
-
-	printf("%d\n", coins);
-	return (0);
+if (argc == 2)
+{
+cents = atoi(*(argv + 1));
+while (cents > 0)
+{
+if (cents % 25 < cents)
+{
+cents -= 25;
+coins++;
+}
+else if (cents % 10 < cents)
+{
+cents -= 10;
+coins++;
+}
+else if (cents % 5 < cents)
+{
+cents -= 5;
+coins++;
+}
+else if (cents % 2 < cents)
+{
+cents -= 2;
+coins++;			}
+else if (cents % 1 < cents)
+{
+cents -= 1;
+coins++;
+}
+}
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", coins);
+return (0);
 }
