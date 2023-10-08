5777e6ac1baf630920bd70c1f079cfcfdc64bbbf
Youstina <youstina@gmail.com>
2023-10-07 17:03:31 -0700

    0X0A C - Argc, Argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,39 @@
+#include "main.h"
+#include "num_check.c"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
- * main -add positive numbers
- * @argc: int
- * @argv: char *
+ * main - add positive numbers
+ * @argc:argument counter
+ * @argv: argument vector
+ *
  * Return: always 0
  */
 
 int main(int argc, char *argv[])
 {
-int i;
-int sum = 0;
+	int i;
+	int sum = 0;
 
-if (argc > 1)
-{
-	for (i = 1; i <= (argc - 1); i++)
+	if (argc == 1)
 	{
-		sum = sum + atoi(argv[i]);
+		printf("0\n");
+		return (0);
+	}
 
+	for (i = 1; i < argc; i++)
+	{
+		if (num_check(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
 	printf("%d\n", sum);
-}
-
-if (argc == 1)
-	printf("0\n");
-
-return (0);
-
+	return (0);
 }
